b632ea8622f11642742515b1d895dbbd87344a88
girvel <widauka@ya.ru>
2025-06-24 15:59:29 +0500

    Dynamically-linked /init

--- init.c
+++ init.c
@@ -5,9 +5,6 @@
 #include <sys/stat.h>
 
 int main(void) {
-    mkdir("/lib64", 0755);
-    rename("/lib/ld-linux-x86-64.so.2", "/lib64/ld-linux-x86-64.so.2");
-
     char *const argv[] = {"lua", "/core/init.lua", NULL};
     char *const envp[] = {NULL};
     execve("lua", argv, envp);
