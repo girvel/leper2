630c92cfb0fddc33375eacf35cac4c08586d4a9f
girvel <widauka@ya.ru>
2025-06-24 15:54:47 +0500

    initramfs/

--- init.c
+++ init.c
@@ -6,15 +6,7 @@
 
 int main(void) {
     mkdir("/lib64", 0755);
-    rename("/ld-linux-x86-64.so.2", "/lib64/ld-linux-x86-64.so.2");
-
-    mkdir("/lib", 0755);
-    mkdir("/lib/x86_64-linux-gnu/", 0755);
-
-    rename("/libc.so.6", "/lib/x86_64-linux-gnu/libc.so.6");
-    rename("/libm.so.6", "/lib/x86_64-linux-gnu/libm.so.6");
-    rename("/libgcc_s.so.1", "/lib/x86_64-linux-gnu/libgcc_s.so.1");
-    rename("/libc.so.6", "/lib/x86_64-linux-gnu/libc.so.6");
+    rename("/lib/ld-linux-x86-64.so.2", "/lib64/ld-linux-x86-64.so.2");
 
     char *const argv[] = {"lua", "/core/init.lua", NULL};
     char *const envp[] = {NULL};
